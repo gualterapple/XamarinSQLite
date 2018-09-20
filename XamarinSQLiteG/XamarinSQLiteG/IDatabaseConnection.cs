8daf9021ad0d619daaebfa1ccf483e67f2396c12
Gualter <gualter_apple@hotmail.com>
2018-09-20 12:49:35 +0100

    Vamos para o mac testar

---
+++ XamarinSQLiteG/XamarinSQLiteG/IDatabaseConnection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinSQLiteG
+{
+    public interface IDatabaseConnection
+    {
+        SQLite.SQLiteConnection DbConnection();
+    }
+}
